f8ae8e7988b463d1235e2e46984602febaaacff0
trbKnl <ncdeschipper@gmail.com>
2024-02-13 13:43:02 +0100

    added compat layer

--- release-script.sh
+++ release-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # build release all individual platforms
-
 script_location='./src/framework/processing/py/port/script.py'
 single_platform='platforms = \[ ("'
 single_platform_commented_out='#platforms = \[ ("'
@@ -9,7 +8,14 @@ single_platform_commented_out='#platforms = \[ ("'
 platforms=("Youtube" "TikTok")
 
 for platform in "${platforms[@]}"; do
-    sed -i "s/$single_platform_commented_out$platform/$single_platform$platform/g" $script_location
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i "s/$single_platform_commented_out$platform/$single_platform$platform/g" $script_location
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/$single_platform_commented_out$platform/$single_platform$platform/g" $script_location
+    else
+        sed -i "s/$single_platform_commented_out$platform/$single_platform$platform/g" $script_location
+    fi
     PLATFORM=$platform npm run release_platform
     git restore $script_location
 done
